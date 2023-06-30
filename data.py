1cfd4314eac339fbe0320682adba12a54a2e4f87
ChrisMajuba <124617351+ChrisMajuba@users.noreply.github.com>
2023-06-30 22:05:51 +0000

    c

--- data.py
+++ data.py
@@ -1,38 +1,31 @@
-import mysql.connector
+from sqlalchemy import create_engine,text, insert
 import os
 
 #retrieve data from the database
 def getMembers():
-  mydb = mysql.connector.connect(
-  host=os.environ['host'],
-  user=os.environ['user'],
-  password=os.environ['password'],
-  database=os.environ['database'],
-  ssl_ca = "/etc/ssl/cert.pem"
-  )
-  mycursor = mydb.cursor()
-  mycursor.execute("SELECT* FROM members;")
-  members = []
-  for m in mycursor.fetchall():
-   members.append(dict(zip(mycursor.column_names,m)))
-  if mycursor != None:
-    mycursor.close()
-  if mydb != None:
-    mydb.close()
+  db_connect_string = os.getenv("database_info")
+  ssl = {
+     "ca": "/etc/ssl/certs/ca-certificates.crt"
+    }
+  engine = create_engine(db_connect_string, connect_args={"ssl": ssl})
+  
+  with engine.connect() as connection:
+    result = connection.execute(text("SELECT *FROM members;"))
+    members = []
+    for row in result.all():
+        members.append(dict(row._mapping))
   return members;
 
 #send data to the database
 def sendApplicant(data):
-  mydb = mysql.connector.connect(
-  host=os.environ['host'],
-  user=os.environ['user'],
-  password=os.environ['password'],
-    database=os.environ['database']
-  )
-  mycursor = mydb.cursor()
-  mycursor.execute("INSERT INTO members(_name,_surname,title,email) VALUES(%s,%s,%s,%s);",(data["name"],data["surname"],"C",data["email"]));
-  mydb.commit()
-  if mycursor != None:
-    mycursor.close()
-  if mydb != None:
-    mydb.close()
+  db_connect_string = os.getenv("database_info")
+  ssl = {
+     "ca": "/etc/ssl/certs/ca-certificates.crt"
+    }
+  engine = create_engine(db_connect_string, connect_args={"ssl": ssl})
+  
+  with engine.connect() as connection:
+    query = text('INSERT INTO members("_name","_surname","title","email") VALUES(:n,:s,:c,:e);')
+    connection.execute(query,n=data["name"],s=data["surname"],c="C",e=data["email"])
+    connection.commit()
+  
