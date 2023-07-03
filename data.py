7bc1cc09feba667d119570b4e56bf2bdd1f75eee
ChrisMajuba <124617351+ChrisMajuba@users.noreply.github.com>
2023-07-03 19:42:07 +0000

    latest c

--- data.py
+++ data.py
@@ -1,14 +1,13 @@
-from sqlalchemy import create_engine,text, insert
+from sqlalchemy import create_engine,text
 import os
 
 #retrieve data from the database
 def getMembers():
   db_connect_string = os.getenv("database_info")
-  ssl = {
-     "ca": "/etc/ssl/certs/ca-certificates.crt"
-    }
-  engine = create_engine(db_connect_string, connect_args={"ssl": ssl})
-  
+  engine = create_engine(db_connect_string,connect_args={
+        "ssl" :{
+    "ssl_ca": "/etc/ssl/cert.pem"}
+    })
   with engine.connect() as connection:
     result = connection.execute(text("SELECT *FROM members;"))
     members = []
@@ -19,13 +18,12 @@ def getMembers():
 #send data to the database
 def sendApplicant(data):
   db_connect_string = os.getenv("database_info")
-  ssl = {
-     "ca": "/etc/ssl/certs/ca-certificates.crt"
-    }
-  engine = create_engine(db_connect_string, connect_args={"ssl": ssl})
-  
-  with engine.connect() as connection:
-    query = text('INSERT INTO members("_name","_surname","title","email") VALUES(:n,:s,:c,:e);')
-    connection.execute(query,n=data["name"],s=data["surname"],c="C",e=data["email"])
-    connection.commit()
+  engine = create_engine(db_connect_string,connect_args={
+        "ssl" :{
+    "ssl_ca": "/etc/ssl/cert.pem"}
+    })
+  with engine.connect() as conn:
+    conn.execute(
+        text("INSERT INTO members (_name, _surname,title,email) VALUES (:n,:s,:c,:e)"),[{"n": data["name"], "s": data["surname"],"c":"C","e":data["email"]}])
+    conn.commit()
   
